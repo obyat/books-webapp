52259b68a7f0d5d2c4a9b21c225029417f0078da
Obyat Abdelrahman <rahman.obyat@gmail.com>
2021-02-17 22:19:16 -0500

    stable version

--- MEMORIES_PROJECT/client/src/api/index.js
+++ MEMORIES_PROJECT/client/src/api/index.js
@@ -6,4 +6,3 @@ export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-git 
