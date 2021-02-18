8b02b612f6d831b4571fa53480d6b1dd416677bd
Obyat Abdelrahman <rahman.obyat@gmail.com>
2021-02-17 22:59:17 -0500

    fixed mobile alignment

--- MEMORIES_PROJECT/client/src/components/Posts/Post/Post.js
+++ MEMORIES_PROJECT/client/src/components/Posts/Post/Post.js
@@ -34,12 +34,12 @@ const Post = ({ post, setCurrentId }) => {
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
         <BookIcon fontSize="small"/>
-            Borrow {' '} 
-            [{post.likeCount}] {' '}
+         Borrow &nbsp;
+            [{post.likeCount}]
         </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
         <RestoreFromTrashIcon fontSize="small"/> 
-            Return 
+        Return &nbsp;
         </Button>
       </CardActions>
     </Card>
